cd29583d1a76fad909ef325e7bbd448a62e7bfd4
musnow <ezplayingd@126.com>
2023-07-07 08:19:13 +0800

    refactor(Gtime): using datetime & zoneinfo for get local time

--- utils/Gtime.py
+++ utils/Gtime.py
@@ -1,23 +1,45 @@
 import time
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 
-#将获取当前时间封装成函数方便使用
 def GetTime():
-    return time.strftime("%y-%m-%d %H:%M:%S", time.localtime())
+    """获取当前时间，格式为 `23-01-01 00:00:00`"""
+    a = datetime.now(ZoneInfo('Asia/Shanghai'))  # 返回北京时间
+    return a.strftime('%y-%m-%d %H:%M:%S')
+    # use time.loacltime if you aren't using BeiJing Time
+    # return time.strftime("%y-%m-%d %H:%M:%S", time.localtime())
 
 
-#将获取当前日期成函数方便使用
-def GetDate():
-    return time.strftime("%y-%m-%d", time.localtime())
+def GetDate(format_str='%y-%m-%d'):
+    """获取当前日期，默认格式为 `23-01-01`"""
+    a = datetime.now(ZoneInfo('Asia/Shanghai'))  # 返回北京时间
+    return a.strftime(format_str)
+    # use time.loacltime if you aren't using BeiJing Time
+    # return time.strftime("%y-%m-%d", time.localtime())
 
 
-# 获取当日早上8点的时间戳（用于计算用户的商店图片是否过期）
 def GetTimeStampOf8AM():
-    return time.mktime(time.strptime(f"{GetDate()} 08:00:00", "%y-%m-%d %H:%M:%S"))
+    """获取当日早上8点的时间戳（用于计算用户的商店图片是否过期）"""
+    return time.mktime(
+        time.strptime(f"{GetDate()} 08:00:00", "%y-%m-%d %H:%M:%S"))
 
 
-def GetTimeFromStamp(timestamp:float|int):
+def GetTimeFromStamp(timestamp: float | int):
     """通过时间戳获取当前的本地时间，格式 23-01-01 00:00:00"""
-    localtime = time.localtime(timestamp)
-    localtime_str = time.strftime("%y-%m-%d %H:%M:%S", localtime)
-    return localtime_str
+    # localtime = time.localtime(timestamp)
+    # localtime_str = time.strftime("%y-%m-%d %H:%M:%S", localtime)
+    a = datetime.fromtimestamp(timestamp, tz=ZoneInfo('Asia/Shanghai'))
+    return a.strftime("%y-%m-%d %H:%M:%S")
+
+
+def GetTimeStamp():
+    """获取当前时间戳"""
+    a = datetime.now(ZoneInfo('Asia/Shanghai'))  # 返回北京时间
+    return a.timestamp()
+
+
+def GetDateTimeNow():
+    """获取东八区的datetime对象"""
+    a = datetime.now(ZoneInfo('Asia/Shanghai'))  # 返回北京时间
+    return a
